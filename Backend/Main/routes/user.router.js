6c6b5ddb74297c5d73d4a59af7069bc95bf8f623
Comicnist099 <marco.ctu@hotmail.com>
2022-11-15 01:31:43 -0600

    Profile Update Finish
    c logro

--- Backend/Main/routes/user.router.js
+++ Backend/Main/routes/user.router.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const UserProduct = require('../services/user.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 const service = new UserProduct();
-let  {container,URL} = require('../../const.json');
+let {container, URL} = require('../../const.json');
 const User = require('../models/user');
-const azureStorage= require("azure-storage");
-const blobService=azureStorage.createBlobService();
+const azureStorage = require("azure-storage");
+const blobService = azureStorage.createBlobService();
 
 
-container=container.split("/")[0]; 
+container = container.split("/")[0];
 /*
  split sirve para separar datos hola.como.estas
  const hola="hola.como.estas"
@@ -17,194 +17,166 @@ container=container.split("/")[0];
  cont dato= hola.split(".")[1];
  console.log(dato) //como
 */
-//shortcut para comentar en bloque: shift + alt + a
+// shortcut para comentar en bloque: shift + alt + a
 
-const {
-  createUserDto,
-  updateUserDto,
-  getUserId,
-} = require('../dtos/users.dto');
+const {createUserDto, updateUserDto, getUserId} = require('../dtos/users.dto');
 
 
-//Sirve para obtener los valores
+// Sirve para obtener los valores
 router.get('/', async (req, res) => {
-  let user = await User.find();//await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
-  let buffer = [];
-  buffer = user;
-  await service.generate(buffer);
-  res.json(user);
+    let user = await User.find(); // await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
+    let buffer = [];
+    buffer = user;
+    await service.generate(buffer);
+    res.json(user);
 
 });
 
 router.get('/Login', async (req, res, next) => {
 
-  try {;
-    const { size, e, p } = req.query;
-    const filter = {};
+    try {;
+        const {size, e, p} = req.query;
+        const filter = {};
 
-    if (e) {
-      Object.assign(filter, {
-        email: e
-      })
+        if (e) {
+            Object.assign(filter, {email: e})
+        }
+
+        if (p) {
+            Object.assign(filter, {password: p})
+        }
+
+        let userSearch = await User.find(filter);
+        const users = await service.find2(size || 10, userSearch)
+
+        res.json({'error': " ", users});
+
+    } catch (error) {
+        next(error);
     }
-
-    if (p) {
-      Object.assign(filter, {
-        password: p
-      })
-    }
-   
-    let userSearch = await User.find(filter);
-    const users = await service.find2(size || 10, userSearch)
-    
-    res.json({
-      'error': " ",
-      users
-    });
-
-  } catch (error) {
-    next(error);
-  }
 
 });
 
 
-router.get('/:id', validatorHandler(getUserId, 'params'),
-  async (req, res, next) => {
+router.get('/:id', validatorHandler(getUserId, 'params'), async (req, res, next) => {
     try {
-      const {
-        id
-      } = req.params; //aquí agarra el id de la url
-      let user = await User.find();//await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
-      let buffer = [];
-      buffer = user;
-      await service.generate(buffer);
-      const users = await service.findOne(id);
+        const {id} = req.params; // aquí agarra el id de la url
+        let user = await User.find(); // await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
+        let buffer = [];
+        buffer = user;
+        await service.generate(buffer);
+        const users = await service.findOne(id);
 
-      res.json(
-        users
-      )
+        res.json(users)
     } catch (error) {
-      next(error);
+        next(error);
 
     }
-  });
+});
 
 
-router.post('/', validatorHandler(createUserDto, 'body'),
-  async (req, res, next) => {
+router.post('/', validatorHandler(createUserDto, 'body'), async (req, res, next) => {
     const {
-      isActive,
-      nameUser,
-      nickname,
-      email,
-      password,
-      creationDate,
-      team,
-      facebook,
-      instagram,
-      extra,
-      image,
-      points,
-      ban,
-      typeUser,
-      achievements1,
-      achievements2,
-      achievements3,
-      achievements4,
-      achievements5,
-      achievements6,
-      achievements7,
-      achievements8,
-      achievements9,
-      achievements10,
-      achievements11
+        isActive,
+        nameUser,
+        nickname,
+        email,
+        password,
+        creationDate,
+        team,
+        facebook,
+        instagram,
+        extra,
+        image,
+        points,
+        ban,
+        typeUser,
+        achievements1,
+        achievements2,
+        achievements3,
+        achievements4,
+        achievements5,
+        achievements6,
+        achievements7,
+        achievements8,
+        achievements9,
+        achievements10,
+        achievements11
     } = req.body;
     const user = new User({
-      isActive,
-      nameUser,
-      nickname,
-      email,
-      password,
-      creationDate,
-      team,
-      facebook,
-      instagram,
-      extra,
-      image,
-      points,
-      ban,
-      typeUser,
-      achievements1,
-      achievements2,
-      achievements3,
-      achievements4,
-      achievements5,
-      achievements6,
-      achievements7,
-      achievements8,
-      achievements9,
-      achievements10,
-      achievements11
+        isActive,
+        nameUser,
+        nickname,
+        email,
+        password,
+        creationDate,
+        team,
+        facebook,
+        instagram,
+        extra,
+        image,
+        points,
+        ban,
+        typeUser,
+        achievements1,
+        achievements2,
+        achievements3,
+        achievements4,
+        achievements5,
+        achievements6,
+        achievements7,
+        achievements8,
+        achievements9,
+        achievements10,
+        achievements11
     });
 
 
-     if (image) {
+    if (image) {
 
-      ({ name, path, extention } = image);
+        ({name, path, extention} = image);
 
-      name = name + "." + extention;
+        name = name + "." + extention;
 
-      let buffer = new Buffer(path, 'base64')
-      await blobService.createBlockBlobFromText(container, name, buffer, {
-        contentType: extention
-      }, async function (err) {
-        if (err) {
+        let buffer = new Buffer(path, 'base64')
+        await blobService.createBlockBlobFromText(container, name, buffer, {
+            contentType: extention
+        }, async function (err) {
+            if (err) {
 
-          res.json({
-            'success': false,
-            'message': err
-          });
+                res.json({'success': false, 'message': err});
 
-        } else {
+            } else {
 
-          const fileURL = `${URL}${container}/${name}`;
+                const fileURL = `${URL}${container}/${name}`;
 
-          user["image"]["path"] = fileURL;
-          await user.save();
-          res.json({
-            'success': true,
-            'message': "El usuario se ha creado con exito",
-            'Data': user
-          });
+                user["image"]["path"] = fileURL;
+                await user.save();
+                res.json({'success': true, 'message': "El usuario se ha creado con exito", 'Data': user});
 
-        }
+            }
 
-      })
+        })
     } else {
-      await user.save();
+        await user.save();
 
-      res.json({
-        'success': true,
-        'message': "El usuario se ha creado con exito",
-        'Data': user
-      });
-    } 
-  });
+        res.json({'success': true, 'message': "El usuario se ha creado con exito", 'Data': user});
+    }
+});
 
-router.patch(
-  '/:id',
-  validatorHandler(getUserId, 'params'),
-  validatorHandler(updateUserDto, 'body'),
-  async (req, res) => {
+router.patch('/:id', validatorHandler(getUserId, 'params'), validatorHandler(updateUserDto, 'body'), async (req, res) => {
 
-    try {
-      const body2 = req.body;
-      const usersM = { // <-- Here
+
+    const body2 = req.body;
+
+
+    const usersM = {
+        // <-- Here
+
         isActive: body2.isActive,
         nameUser: body2.nameUser,
         nickname: body2.nickname,
-        email:body2.email,
+        email: body2.email,
         password: body2.password,
         creationDate: body2.creationDate,
         team: body2.team,
@@ -226,48 +198,65 @@ router.patch(
         achievements9: body2.achievements9,
         achievements10: body2.achievements10,
         achievements11: body2.achievements11
-      }
-      let user = await User.find();//await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
-      let buffer = [];
-      buffer = user;
-      await service.generate(buffer);
-      await service.update(req.params.id, body2);
-      await User.findByIdAndUpdate(req.params.id, usersM);
-      res.json(
-        usersM
-      );
-    } catch (error) {
-      res.status(404).json({
-        message: error.message,
-      })
     }
-  });
+    if (body2.image) {
 
-router.delete('/:id', validatorHandler(getUserId, 'params'),
-  async (req, res) => {
+        ({name, path, extention} = body2.image);
+
+        name = name + "." + extention;
+
+        let buffer = new Buffer(path, 'base64')
+        await blobService.createBlockBlobFromText(container, name, buffer, {
+            contentType: extention
+        }, async function (err) {
+            if (err) {
+
+                res.json({'success': false, 'message': err});
+
+            } else {
+
+                const fileURL = `${URL}${container}/${name}`;
+
+                usersM["image"]["path"] = fileURL;
+                await User.findByIdAndUpdate(req.params.id, usersM);
+                res.json({'success': true, 'message': "El usuario se ha creado con exito", 'Data': usersM});
+
+            }
+
+        })
+    } else {
+        await User.findByIdAndUpdate(req.params.id, usersM);
+
+        res.json({'success': true, 'messaage': "El usuario se ha creado con exito", 'Data': usersM});
+    }
+    /*  let user = await User.find(); 
+        let buffer = [];
+        buffer = user;
+        await service.generate(buffer);
+        await service.update(req.params.id, body2);
+        await User.findByIdAndUpdate(req.params.id, usersM);
+        res.json(usersM);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    } */
+});
+
+router.delete('/:id', validatorHandler(getUserId, 'params'), async (req, res) => {
 
     try {
-      const {
-        id
-      } = req.params;//let es lo contrario a const
-      let user = await User.find();//await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
-      let buffer = [];
-      buffer = user;
-      await service.generate(buffer);
-      const deleteUser = await service.delete(id);
-      await User.findByIdAndRemove(id);
-      res.json({
-        message: 'delete',
-        product: deleteUser,
-        id,
-      });
+        const {id} = req.params; // let es lo contrario a const
+        let user = await User.find(); // await sirve para q se espere antes de realizar la funcion y se pueda ejecutar correctamente
+        let buffer = [];
+        buffer = user;
+        await service.generate(buffer);
+        const deleteUser = await service.delete(id);
+        await User.findByIdAndRemove(id);
+        res.json({message: 'delete', product: deleteUser, id});
     } catch (error) {
-      res.status(404).json({
-        message: error.message,
-      });
+        res.status(404).json({message: error.message});
 
     }
-  });
+});
 
 
 module.exports = router;
